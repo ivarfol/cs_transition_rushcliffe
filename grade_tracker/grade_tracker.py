5e9d2d497aeb017885b48e2639d4ae2d14821c93
Iaroslav Varfolomeev <varfolomeeviaroslav@gmail.com>
2024-08-26 00:01:57 +0100

    add grade tracker

---
+++ grade_tracker/grade_tracker.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 25 18:42:19 2024
+
+@author: iaroslav
+"""
+import pickle
+import os
+from grade_tracker_config_reader import get_settings
+
+def main():
+    config = get_settings()
+    def_path = config.path
+    new = config.new
+    if new == 'yes':
+        student_dict = {}
+    else:
+        student_dict = load(def_path)
+    while True:
+        command = command_valid('Input a command\nadd student/add score/revert score/remove student/scores/save/exit\n')
+        if command[:4] == 'exit':
+            if binary_choice('Do you want to save the changes?'):
+                save(def_path, student_dict)
+            break
+        elif command[:11] == 'add student':
+            student_dict.update({command[12:]: []})
+        elif command[:9] == 'add score':
+            student_dict = add_score(student_dict, command[10:])
+        elif command[:12] == 'revert score':
+            student_dict = revert_score(student_dict, command[13:])
+        elif command[:14] == 'remove student':
+            student_dict = remove_student(student_dict, command[15:])
+        elif command[:4] == 'save':
+            save(def_path, student_dict)
+        elif command[:6] == 'scores':
+            if binary_choice('Do you want to sort the dictionary?'):
+                scores_print(scores_sort(get_avrg(student_dict)))
+            else:
+                scores_print(get_avrg(student_dict))
+    return()
+
+def load(def_path):
+    if os.path.exists(def_path):
+        with open(def_path,'rb') as file:
+            st_dict = pickle.load(file)
+        return(st_dict)
+    dict_path = input('Path\n')
+    while not os.path.exists(dict_path):
+        dict_path = input('Path\n')
+    with open(dict_path,'rb') as file:
+        st_dict = pickle.load(file)
+    return(st_dict)
+    
+def save(def_path, st_dict):
+    if os.path.exists(def_path):
+        with open(def_path,'wb') as file:
+            pickle.dump(st_dict, file)
+        return()
+    dict_path = input('Path\n')
+    while not os.path.exists(dict_path):
+        dict_path = input('Path\n')
+    with open(dict_path,'wb') as file:
+        pickle.dump(st_dict, file)
+    return()
+
+def command_valid(message):
+    command = input(message)
+    while (command[:11] != 'add student' or len(command) < 12) and (command[:9] != 'add score' or command.count(' ') < 3) and (command[:12] != 'revert score' or len(command) < 13) and (command[:14] != 'remove student' or len(command) < 15) and command[:6] != 'scores' and command[:4] != 'save' and command[:4] != 'exit':
+        command = input(message)
+    return(command)
+
+def add_score(st_dict, inp):
+    inp = inp.split()
+    if len(inp) != 2:
+        return(st_dict)
+    if not inp[0] in st_dict.keys():
+        if binary_choice('Are you shure you want to add a student?'):
+            st_dict.update({inp[0]: []})
+        else:
+            return(st_dict)
+    score = inp_num('Input score:\n', inp[1])
+    st_dict[inp[0]] = st_dict[inp[0]] + [score]
+    return(st_dict)
+
+def revert_score(st_dict, inp):
+    if not inp in st_dict.keys():
+        return(st_dict)
+    if len(st_dict[inp]) >= 1:
+        st_dict[inp] = st_dict[inp][:-1]
+    return(st_dict)
+
+def remove_student(st_dict, inp):
+    if inp in st_dict.keys():
+        st_dict.pop(inp)
+    return(st_dict)
+
+def inp_num(message, inp):
+    while True:
+        try:
+            score = float(inp)
+            break
+        except:
+            print('Has to be float or integer')
+            score = float(input(message))
+        inp = input()
+    return(score)
+
+def get_avrg(st_dict):
+    temp = st_dict.items()
+    temp_dict = {}
+    temp_pair = []
+    avrg_temp = 0
+    for pair in temp:
+        avrg_temp = 0
+        temp_pair = [pair[0]]
+        for score in pair[1]:
+            avrg_temp += score
+        if len(pair[1]) != 0:
+            temp_pair += [avrg_temp / len(pair[1])]
+        else:
+            temp_pair += [0]
+        temp_dict[temp_pair[0]] = temp_pair[1]
+    return(temp_dict)
+
+def scores_sort(temp_dict):
+    return(dict(sorted(temp_dict.items(), key=lambda item: item[1], reverse=True)))
+
+def scores_print(temp_dict):
+    temp = temp_dict.items()
+    for pair in temp:
+        print(pair[0], end=': ')
+        print(pair[1])
+    return()
+
+def binary_choice(message):
+    choice = input(f'{message}\ny/n\n')
+    while choice not in {'y', 'n'}:
+        choice = input(f'{message}\ny/n\n')
+    if choice == 'y':
+        return(True)
+    else:
+        return(False)
+
+if __name__ == '__main__':
+    main()
---
+++ grade_tracker/grade_tracker_config_reader.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 25 22:21:10 2024
+
+@author: iaroslav
+"""
+from enum import Enum
+from os import path
+from typing import Tuple, Type
+
+from pydantic import Field
+from pydantic_settings import (
+    BaseSettings,
+    PydanticBaseSettingsSource,
+    SettingsConfigDict,
+    YamlConfigSettingsSource,
+)
+class YesNoEnum(str, Enum):
+    YES = "yes"
+    NO = "no"
+    
+def get_config_file():
+    config_file = path.join(path.abspath(path.dirname(__file__)), 'config_grade_tracker.yaml')
+    if not path.isfile(config_file):
+        raise ValueError(
+            f"Config file ({config_file}) is missing"
+        )
+    return config_file
+
+class Settings(BaseSettings, case_sensitive=True):
+    model_config = SettingsConfigDict(yaml_file=get_config_file())
+    path: str = Field(default='')
+    new: YesNoEnum = Field(default=YesNoEnum.NO)
+
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls: Type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> Tuple[PydanticBaseSettingsSource, ...]:
+        return env_settings, dotenv_settings, YamlConfigSettingsSource(settings_cls)
+    
+def get_settings() -> Settings:
+    if get_config_file():
+        return Settings()  # type: ignore
+    else:
+        raise FileNotFoundError("Config directory not found")
